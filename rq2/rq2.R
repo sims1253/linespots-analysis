fadd226d3e10fa5e792c69037878bdd71760d8c7
Maximilian Scholz <m0hawk@gmx.de>
2019-08-09 08:10:56 +0200

    Final rq2 models

--- rq2/rq2.R
+++ rq2/rq2.R
@@ -208,16 +208,8 @@ loo1.6 = loo(m1.6)
 loo1.7 = loo(m1.7)
 
 loo_compare(loo1.1, loo1.2, loo1.3, loo1.4, loo1.5, loo1.6, loo1.7)
-# Using se_diff > 4*elpd_diff for a significantly better model, models 3 to 7 are
-# essentially equally good.
-# In that case, model 3 would be the preferred one as it has the fewest predictors.
-# We can also keep model 7 for comparison.
-# And while we see transitions exceeding tree depth, that is not a validity concern
-# but only a efficiency concern.
-summary(m1.3)
-summary(m1.7)
-# Model 7 seems to sample better. 3 has one rhat at 1.02
-
+# Models 3-7 are essentially identical in loo so we go with the simplest one
+# that samples properly.
 save(m1.1, m1.2, m1.3, m1.4, m1.5, m1.6, m1.7, file="m1.RData")
 
 
@@ -346,19 +338,14 @@ loo2.6 = loo(m2.6)
 loo2.7 = loo(m2.7)
 
 loo_compare(loo2.1, loo2.2, loo2.3, loo2.4, loo2.5, loo2.6, loo2.7)
-# Again models 3 to 7 have essentially the same loo performance.
-# We again chose model 3 as it is the simplest one. We can keep model 7 as comparison.
-
-summary(m2.3)
-summary(m2.7)
-# Again model 7 seems to sample better
-
+# Models 3-7 are essentially identical in loo so we go with the simplest one
+# that samples properly.
 save(m2.1, m2.2, m2.3, m2.4, m2.5, m2.6, m2.7, file="m2.RData")
 
 ##########################################################
 # Final models
 
-# based on 1.3
+# based on m1.3
 m3.1 = brm(
   formula = EXAM ~ 1 + Time + LOC + (1|Project),
   data = ls.df,
@@ -374,19 +361,25 @@ m3.1 = brm(
   chains = 4,
   cores = parallel::detectCores(),
   sample_prior = TRUE,
-  control = list(adapt_delta=0.999),
+  control = list(adapt_delta=0.999, max_treedepth=15),
   seed = SEED
 )
 
-# based on 1.7
+loo3.1 = loo(m3.1)
+plot(m3.1)
+summary(m3.1)
+# The intercept doesn't sample well. We try the least complex model that has over 10% n_eff
+# for all coefficients.
+
+# based on m1.4
 m3.2 = brm(
-  formula = EXAM ~ 1 + Time + LOC + Origin + (1|Project) + (1|Domain) + (1|Language),
+  formula = EXAM ~ 1 + Time + LOC + (1|Project) + (1|Language),
   data = ls.df,
   family=Beta(),
   prior = c(
-    prior(normal(0,1), class=Intercept),
+    prior(normal(0,0.5), class=Intercept),
     prior(normal(0,0.5), class=b),
-    prior(cauchy(0,0.5), class=sd),
+    prior(cauchy(0,0.1), class=sd),
     prior(gamma(0.1, 0.1), class=phi)
   ),
   iter = 10000,
@@ -394,40 +387,15 @@ m3.2 = brm(
   chains = 4,
   cores = parallel::detectCores(),
   sample_prior = TRUE,
-  control = list(adapt_delta=0.999),
+  control = list(adapt_delta=0.999, max_treedepth=15),
   seed = SEED
 )
-save(m3.1, m3.2, file="m3.RData")
-
-loo_compare(loo(m3.1), loo(m3.2))
-
-summary(m3.1)
 summary(m3.2)
-plot(m3.1)
 plot(m3.2)
+loo3.2 = loo(m3.2)
+loo_compare(loo3.1, loo3.2)
 
-# Model 2 seems to sample a little better. Especially the Intercept.
-# Regardless, both seem to 
-
-stanplot(m3.1, type="hist")
-stanplot(m3.1, type="dens_overlay")
-stanplot(m3.1, type="areas")
-stanplot(m3.1, type="areas", pars="b_")
-stanplot(m3.1, type="areas", pars="sd_")
-
-np <- nuts_params(m3.1)
-lp <- log_posterior(m3.1)
-mcmc_nuts_acceptance(np, lp)
-mcmc_nuts_divergence(np, lp)
-mcmc_nuts_stepsize(np, lp)
-mcmc_nuts_treedepth(np, lp)
-mcmc_nuts_energy(np, lp)
-# From what I understand, the sampling seems to work fine.
-
-plot(hypothesis(m3.1, "Timelinear_Time_function=0"))
-plot(hypothesis(m3.1, "Timeflat_Time_function=0"))
-
-
+save(m3.1, m3.2, file="m3.RData")
 ############################################################
 # And do the same for the AUCEC
 
@@ -437,9 +405,9 @@ m4.1 = brm(
   data = ls.df,
   family=Beta(),
   prior = c(
-    prior(normal(0,1), class=Intercept),
+    prior(normal(0,0.5), class=Intercept),
     prior(normal(0,0.5), class=b),
-    prior(cauchy(0,0.5), class=sd),
+    prior(cauchy(0,0.1), class=sd),
     prior(gamma(0.1, 0.1), class=phi)
   ),
   iter = 10000,
@@ -447,19 +415,20 @@ m4.1 = brm(
   chains = 4,
   cores = parallel::detectCores(),
   sample_prior = TRUE,
-  control = list(adapt_delta=0.999),
+  control = list(adapt_delta=0.999, max_treedepth=15),
   seed = SEED
 )
+summary(m4.1)
+loo4.1 = loo(m4.1)
 
-# Based on 2.7
 m4.2 = brm(
-  formula = AUCECEXAM ~ 1 + Time + LOC + Origin + (1|Project) + (1|Domain) + (1|Language),
+  formula = AUCECEXAM ~ 1 + Time + LOC + (1|Project) + (1|Language),
   data = ls.df,
   family=Beta(),
   prior = c(
-    prior(normal(0,1), class=Intercept),
+    prior(normal(0,0.5), class=Intercept),
     prior(normal(0,0.5), class=b),
-    prior(cauchy(0,0.5), class=sd),
+    prior(cauchy(0,0.1), class=sd),
     prior(gamma(0.1, 0.1), class=phi)
   ),
   iter = 10000,
@@ -467,34 +436,13 @@ m4.2 = brm(
   chains = 4,
   cores = parallel::detectCores(),
   sample_prior = TRUE,
-  control = list(adapt_delta=0.999),
+  control = list(adapt_delta=0.999, max_treedepth=15),
   seed = SEED
 )
-save(m4.1, m4.2, file="m4.RData")
-
-summary(m4.1)
 summary(m4.2)
-# Model 1 seems to sample a lot better but the Time coefficients are the same for both.
-plot(m4.1)
-plot(m4.2)
+loo4.2 = loo(m4.2)
 
-loo_compare(loo(m4.1), loo(m4.2))
-stanplot(m3.2, type="hist")
-stanplot(m3.2, type="dens_overlay")
-stanplot(m3.2, type="areas")
-stanplot(m3.2, type="areas", pars="b_")
-stanplot(m3.2, type="areas", pars="sd_")
-
-np <- nuts_params(m3.2)
-lp <- log_posterior(m3.2)
-mcmc_nuts_acceptance(np, lp)
-mcmc_nuts_divergence(np, lp)
-mcmc_nuts_stepsize(np, lp)
-mcmc_nuts_treedepth(np, lp)
-mcmc_nuts_energy(np, lp)
-
-plot(hypothesis(m3.2, "Timelinear_Time_function=0"))
-plot(hypothesis(m3.2, "Timeflat_Time_function=0"))
-
+loo_compare(loo4.1, loo4.2)
 
 save(m4.1, m4.2, file="m4.RData")
+
